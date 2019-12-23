a01e2c8286566504dc8d7f1dff950bd99f0fb5b6
Andrew Dibble <acdibble@gmail.com>
2019-12-23 07:37:43 -0600

    Comlete day 21

---
+++ day21/part1.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const intcodeComputer = require('../intcodeComputer');
+
+const intcodes = fs.readFileSync(`${__dirname}/data.txt`, 'utf8').split(',').map(Number);
+const instructions = fs.readFileSync(`${__dirname}/part1.icc`, 'utf8')
+  .split('')
+  .map((c) => c.charCodeAt(0));
+
+console.log(intcodes);
+
+const outputs = [];
+
+let damage = 0;
+
+const run = intcodeComputer(() => {
+  if (outputs.length) {
+    console.log(outputs.splice(0, Infinity).join(''));
+  }
+  return instructions.shift();
+}, (a) => {
+  if (a <= 126) {
+    outputs.push(String.fromCharCode(a));
+  } else {
+    damage += a;
+  }
+});
+
+run(intcodes);
+
+fs.writeFileSync(`${__dirname}/out.txt`, outputs.join(''), 'utf8');
+console.log(damage);
