e368d71ed7540eca0829196404d71f91828cb07d
Andrew Dibble <acdibble@gmail.com>
2020-03-30 00:02:04 +0200

    Parse nums inline

--- 2018/rs/day01/part2.rs
+++ 2018/rs/day01/part2.rs
@@ -2,19 +2,12 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
-fn parse_frequencies(reader: BufReader<File>) -> Vec<i32> {
-    let mut nums = Vec::<i32>::new();
-
-    for line in reader.lines() {
-        nums.push(line.unwrap().parse().unwrap());
-    }
-
-    nums
-}
-
 fn main() {
-    let reader = BufReader::new(File::open("day01/input.txt").unwrap());
-    let nums = parse_frequencies(reader);
+    let nums: Vec<i32> = BufReader::new(File::open("day01/input.txt").unwrap())
+        .lines()
+        .map(|line| line.unwrap().parse().unwrap())
+        .collect();
+
     let mut frequencies = HashSet::new();
     let mut total = 0;
 
