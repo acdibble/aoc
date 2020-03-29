8cf6336de5d8b2d103b84e7c6cc05aba1d8e1efa
Andrew Dibble <acdibble@gmail.com>
2020-03-29 23:50:35 +0200

    Simplify types

--- 2018/rs/day01/part2.rs
+++ 2018/rs/day01/part2.rs
@@ -2,11 +2,11 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
-fn parse_frequencies(reader: BufReader<File>) -> Vec::<i32> {
+fn parse_frequencies(reader: BufReader<File>) -> Vec<i32> {
     let mut nums = Vec::<i32>::new();
 
     for line in reader.lines() {
-        nums.push(line.unwrap().parse::<i32>().unwrap());
+        nums.push(line.unwrap().parse().unwrap());
     }
 
     nums
@@ -15,7 +15,7 @@ fn parse_frequencies(reader: BufReader<File>) -> Vec::<i32> {
 fn main() {
     let reader = BufReader::new(File::open("day01/input.txt").unwrap());
     let nums = parse_frequencies(reader);
-    let mut frequencies = HashSet::<i32>::new();
+    let mut frequencies = HashSet::new();
     let mut total = 0;
 
     loop {
