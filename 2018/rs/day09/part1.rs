f8fcb6abde3304ff697eace5e26305f77cc48dce
Andrew Dibble <andrew@dibble.codes>
2020-05-24 21:37:14 +0200

    Complete 2018/9/1 in rust

---
+++ 2018/rs/day09/part1.rs
@@ -0,0 +1,24 @@
+use std::collections::VecDeque;
+
+const NUMBER_OF_PLAYERS: i32 = 405;
+const NUMBER_OF_MARBLES: i32 = 71700;
+
+fn main() {
+    let mut marbles = VecDeque::new();
+    marbles.push_back(0);
+    let mut scores = vec![0; NUMBER_OF_PLAYERS as usize];
+
+    for next_marble in 1..=NUMBER_OF_MARBLES {
+        if next_marble % 23 != 0 {
+            marbles.rotate_left(1);
+            marbles.rotate_left(1);
+            marbles.push_front(next_marble);
+        } else {
+            marbles.rotate_right(7);
+            scores[(next_marble % NUMBER_OF_PLAYERS) as usize] +=
+                marbles.pop_front().unwrap() + next_marble;
+        }
+    }
+
+    println!("{}", scores.iter().max().unwrap());
+}
