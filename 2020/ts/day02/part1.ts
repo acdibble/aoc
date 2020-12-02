82cf128916d8f3fd1ed119dd9f709a11e63febef
Andrew Dibble <andrew@dibble.codes>
2020-12-02 12:58:36 +0100

    Simplify

--- 2020/ts/day02/part1.ts
+++ 2020/ts/day02/part1.ts
@@ -1,27 +1,17 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { createInterface } from 'readline';
 
-(async () => {
-  const stream = fs.createReadStream(path.join(__dirname, 'data.txt'), 'utf8');
-  const rl = createInterface(stream);
-  stream.on('end', () => rl.close());
-
-  let validCount = 0;
-  for await (const line of rl) {
-    const {
-      min,
-      max,
-      letter,
-      password,
+const validPasswordCount = fs.readFileSync(path.join(__dirname, 'data.txt'), 'utf8').split('\n').reduce((acc, line) => {
+  const {
+    min,
+    max,
+    letter,
+    password,
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    } = /^(?<min>\d+)-(?<max>\d+) (?<letter>[a-z]): (?<password>[a-z]+)$/.exec(line)!.groups!;
+  } = /^(?<min>\d+)-(?<max>\d+) (?<letter>[a-z]): (?<password>[a-z]+)$/.exec(line)!.groups!;
 
-    const difference = password.length - password.replace(new RegExp(`(${letter})`, 'g'), '').length;
-    if (difference >= Number(min) && difference <= Number(max)) {
-      validCount += 1;
-    }
-  }
+  const difference = password.length - password.replace(new RegExp(`(${letter})`, 'g'), '').length;
+  return acc + Number(difference >= Number(min) && difference <= Number(max));
+}, 0);
 
-  console.log(validCount);
-})();
+console.log(validPasswordCount);
--- 2020/ts/day02/part2.ts
+++ 2020/ts/day02/part2.ts
@@ -1,25 +1,17 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { createInterface } from 'readline';
 
-(async () => {
-  const stream = fs.createReadStream(path.join(__dirname, 'data.txt'), 'utf8');
-  const rl = createInterface(stream);
-  stream.on('end', () => rl.close());
-
-  let validCount = 0;
-  for await (const line of rl) {
-    const {
-      min,
-      max,
-      letter,
-      password,
+const validPasswordCount = fs.readFileSync(path.join(__dirname, 'data.txt'), 'utf8').split('\n').reduce((acc, line) => {
+  const {
+    min,
+    max,
+    letter,
+    password,
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    } = /^(?<min>\d+)-(?<max>\d+) (?<letter>[a-z]): (?<password>[a-z]+)$/.exec(line)!.groups!;
+  } = /^(?<min>\d+)-(?<max>\d+) (?<letter>[a-z]): (?<password>[a-z]+)$/.exec(line)!.groups!;
 
-    const count = Number(password[Number(min) - 1] === letter) + Number(password[Number(max) - 1] === letter);
-    if (count === 1) validCount += 1;
-  }
+  const count = Number(password[Number(min) - 1] === letter) + Number(password[Number(max) - 1] === letter);
+  return acc + Number(count === 1);
+}, 0);
 
-  console.log(validCount);
-})();
+console.log(validPasswordCount);
