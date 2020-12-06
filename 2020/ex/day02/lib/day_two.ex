2d3d9a3b6c030a97241a42e2c729a52d9dc4c217
Andrew Dibble <andrew@dibble.codes>
2020-12-06 12:28:04 +0100

    Clean up

--- 2020/ex/day02/lib/day_two.ex
+++ 2020/ex/day02/lib/day_two.ex
@@ -8,56 +8,51 @@ defmodule Rule do
       :bounds ->
         occurrences >= min and occurrences <= max
 
-      _ ->
+      :indices ->
         String.at(password, min - 1) == <<letter>> != (String.at(password, max - 1) == <<letter>>)
     end
-    |> case do
-      true -> 1
-      _ -> 0
-    end
+    |> bool_to_num()
   end
 
-  defp count_occurrences(rule, "", count) do
-    IO.inspect([rule, count])
-    count
-  end
+  defp count_occurrences(_rule, "", count), do: count
 
   defp count_occurrences(%Rule{letter: letter} = rule, <<char, rest::binary>>, count) do
     new_count =
-      case char == letter do
-        true -> 1
-        _ -> 0
-      end
+      (char == letter)
+      |> bool_to_num()
       |> Kernel.+(count)
 
     count_occurrences(rule, rest, new_count)
   end
 
-  def parse_string(string), do: String.trim(string) |> parse_rule(%Rule{}, "")
+  def from_string(string), do: String.trim(string) |> from_string(%Rule{}, "")
 
-  defp parse_rule("", rule, string), do: %{rule | password: string}
+  defp from_string("", rule, string), do: %{rule | password: string}
 
-  defp parse_rule(<<"-", rest::binary>>, rule, string) do
-    parse_rule(rest, %{rule | min: String.to_integer(string)}, "")
+  defp from_string(<<"-", rest::binary>>, rule, string) do
+    from_string(rest, %{rule | min: String.to_integer(string)}, "")
   end
 
-  defp parse_rule(<<":", rest::binary>>, rule, <<char, "">>) do
-    parse_rule(rest, %{rule | letter: char}, "")
+  defp from_string(<<":", rest::binary>>, rule, <<char, "">>) do
+    from_string(rest, %{rule | letter: char}, "")
   end
 
-  defp parse_rule(<<" ", rest::binary>>, %Rule{letter: letter} = rule, string) do
+  defp from_string(<<" ", rest::binary>>, %Rule{letter: letter} = rule, string) do
     update =
       case letter do
         '' -> %{rule | max: String.to_integer(string)}
         _ -> rule
       end
 
-    parse_rule(rest, update, "")
+    from_string(rest, update, "")
   end
 
-  defp parse_rule(<<char, rest::binary>>, rule, string) do
-    parse_rule(rest, rule, string <> <<char>>)
+  defp from_string(<<char, rest::binary>>, rule, string) do
+    from_string(rest, rule, string <> <<char>>)
   end
+
+  defp bool_to_num(true), do: 1
+  defp bool_to_num(false), do: 0
 end
 
 defmodule DayTwo do
@@ -70,15 +65,15 @@ defmodule DayTwo do
 
   def part_one do
     load_file("../data.txt")
-    |> Stream.map(&Rule.parse_string/1)
+    |> Stream.map(&Rule.from_string/1)
     |> Stream.map(&Rule.validate(&1, :bounds))
-    |> Enum.reduce(0, &Kernel.+/2)
+    |> Enum.sum()
   end
 
   def part_two do
     load_file("../data.txt")
-    |> Stream.map(&Rule.parse_string/1)
+    |> Stream.map(&Rule.from_string/1)
     |> Stream.map(&Rule.validate(&1, :indices))
-    |> Enum.reduce(0, &Kernel.+/2)
+    |> Enum.sum()
   end
 end
