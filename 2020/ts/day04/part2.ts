54b3b99f255c9c5bba93205d7a0dfa27fd0c9c28
Andrew Dibble <andrew@dibble.codes>
2020-12-06 18:26:25 +0100

    Shorten the monster

--- 2020/ts/day04/part2.ts
+++ 2020/ts/day04/part2.ts
@@ -1,7 +1,7 @@
 import { path } from "../deps.ts";
 
 const monster =
-  /(?=(?:.|[^\n]\n)*byr:(?:19[2-9][0-9]|200[012])(?:\s|$))(?=(?:.|[^\n]\n)*iyr:20(?:1[0-9]|20)(?:\s|$))(?=(?:.|[^\n]\n)*eyr:20(?:2[0-9]|30)(?:\s|$))(?=(?:.|[^\n]\n)*hgt:(?:(?:59|6[0-9]|7[0-6])in|1(?:(?:[5-8][0-9]|9[0-3])cm))(?:\s|$))(?=(?:.|[^\n]\n)*hcl:#[0-9a-f]{6}(?:\s|$))(?=(?:.|[^\n]\n)*ecl:(?:amb|blu|brn|gry|grn|hzl|oth)(?:\s|$))(?=(?:.|[^\n]\n)*pid:[0-9]{9}(?:\s|$))([\w\W]+?)(?:\n\n|$)/g;
+  /(?=(.\n?)*byr:(19[2-9][0-9]|200[012])\b)(?=(.\n?)*iyr:20(1[0-9]|20)\b)(?=(.\n?)*eyr:20(2[0-9]|30)\b)(?=(.\n?)*hgt:((59|6[0-9]|7[0-6])in|1(([5-8][0-9]|9[0-3])cm))\b)(?=(.\n?)*hcl:#[0-9a-f]{6}\b)(?=(.\n?)*ecl:(amb|blu|brn|gry|grn|hzl|oth)\b)(?=(.\n?)*pid:[0-9]{9}\b)([\w\W]+?)(\n\n|$)/g;
 
 const count = (await Deno.readTextFile(
   path.join(path.fromFileUrl(path.dirname(import.meta.url)), "data.txt"),
