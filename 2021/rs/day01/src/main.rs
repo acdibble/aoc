39a10ef1209eb8c112f2a47352461f7f0d3fa422
Andrew Dibble <andrew@dibble.codes>
2021-12-01 17:22:55 +0100

    [rs] Day 1 2021

--- 2021/rs/day01/src/main.rs
+++ 2021/rs/day01/src/main.rs
@@ -3,12 +3,25 @@ use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
 
-fn part_one(input: &str) -> i32 {
-    0
+fn part_one(measurements: &Vec<i32>) -> i32 {
+    measurements.windows(2).fold(0, |acc, slice| {
+        acc + if slice[0] < slice[1] { 1 } else { 0 }
+    })
 }
 
-fn part_two(input: &str) -> i32 {
-    0
+fn part_two(measurements: &Vec<i32>) -> i32 {
+    let mut previous = 0;
+    let mut increases = -1;
+
+    for window in measurements.windows(3) {
+        let sum = window.iter().sum();
+        if sum > previous {
+            increases += 1;
+        }
+        previous = sum;
+    }
+
+    increases
 }
 
 fn time_it<F, T>(fun: F) -> T
@@ -23,11 +36,12 @@ where
 
 fn main() -> std::io::Result<()> {
     let file_path = env::current_dir()?.join(Path::new("data.txt"));
-    let input = fs::read_to_string(file_path)?;
 
-    time_it(|| println!("part 1: {}", part_one(&input)));
-    time_it(|| println!("part 2: {}", part_two(&input)));
+    let input = fs::read_to_string(file_path)?;
+    let measurements = input.lines().map(|l| l.parse::<i32>().unwrap()).collect();
+
+    time_it(|| println!("part 1: {}", part_one(&measurements)));
+    time_it(|| println!("part 2: {}", part_two(&measurements)));
 
     Ok(())
 }
-
