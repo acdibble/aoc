067ae426a93244358d5f98b405b043007f2e96f0
Andrew Dibble <andrew@dibble.codes>
2021-12-07 18:09:45 +0100

    [rs] Day 7 2021

--- 2021/rs/day07/src/main.rs
+++ 2021/rs/day07/src/main.rs
@@ -3,12 +3,37 @@ use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
 
-fn part_one(input: &str) -> i32 {
-    0
-}
+fn solve(input: &str) -> (i32, i32) {
+    let crabs: Vec<i32> = input
+        .split(',')
+        .map(|string| string.parse().unwrap())
+        .collect();
 
-fn part_two(input: &str) -> i32 {
-    0
+    let min = *crabs.iter().min().unwrap();
+    let max = *crabs.iter().max().unwrap();
+
+    let mut part_one = i32::MAX;
+    let mut part_two = i32::MAX;
+
+    for position in min..=max {
+        let mut current_part_one = 0;
+        let mut current_part_two = 0;
+
+        for crab in &crabs {
+            let distance = (position - crab).abs();
+            current_part_one += distance;
+            current_part_two += (distance * (distance + 1)) / 2;
+        }
+
+        if current_part_one > part_one && current_part_two > part_two {
+            break;
+        }
+
+        part_one = part_one.min(current_part_one);
+        part_two = part_two.min(current_part_two);
+    }
+
+    (part_one, part_two)
 }
 
 fn time_it<F, T>(fun: F) -> T
@@ -25,9 +50,7 @@ fn main() -> std::io::Result<()> {
     let file_path = env::current_dir()?.join(Path::new("data.txt"));
     let input = fs::read_to_string(file_path)?;
 
-    time_it(|| println!("part 1: {}", part_one(&input)));
-    time_it(|| println!("part 2: {}", part_two(&input)));
+    time_it(|| println!("part (1, 2): {:?}", solve(&input)));
 
     Ok(())
 }
-
