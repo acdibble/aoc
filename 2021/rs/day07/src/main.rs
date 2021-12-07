c5ebec8f658bae2c44679b475f6b99cee86e4659
Andrew Dibble <andrew@dibble.codes>
2021-12-07 21:27:55 +0100

    [rs] Day 7 2021 - binary search

--- 2021/rs/day07/src/main.rs
+++ 2021/rs/day07/src/main.rs
@@ -3,37 +3,59 @@ use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
 
+fn simple_fuel_consumption(crabs: &Vec<i32>, position: i32) -> i32 {
+    crabs
+        .iter()
+        .fold(0, |acc, crab| acc + (position - crab).abs())
+}
+
+fn summation_fuel_consumption(crabs: &Vec<i32>, position: i32) -> i32 {
+    crabs.iter().fold(0, |acc, crab| {
+        let distance = (position - crab).abs();
+        acc + (distance * (distance + 1)) / 2
+    })
+}
+
+fn binary_search(
+    crabs: &Vec<i32>,
+    mut left: i32,
+    mut right: i32,
+    fuel_fn: fn(&Vec<i32>, i32) -> i32,
+) -> i32 {
+    let mut lowest_amount = i32::MAX;
+
+    while left < right {
+        let mid = (left + right) / 2;
+
+        let current_result = fuel_fn(&crabs, mid);
+        let right_result = fuel_fn(&crabs, mid + 1);
+
+        lowest_amount = lowest_amount.min(current_result);
+        lowest_amount = lowest_amount.min(current_result);
+
+        if right_result >= current_result {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    lowest_amount
+}
+
 fn solve(input: &str) -> (i32, i32) {
     let crabs: Vec<i32> = input
         .split(',')
         .map(|string| string.parse().unwrap())
         .collect();
 
-    let min = *crabs.iter().min().unwrap();
-    let max = *crabs.iter().max().unwrap();
+    let left = *crabs.iter().min().unwrap();
+    let right = *crabs.iter().max().unwrap();
 
-    let mut part_one = i32::MAX;
-    let mut part_two = i32::MAX;
-
-    for position in min..=max {
-        let mut current_part_one = 0;
-        let mut current_part_two = 0;
-
-        for crab in &crabs {
-            let distance = (position - crab).abs();
-            current_part_one += distance;
-            current_part_two += (distance * (distance + 1)) / 2;
-        }
-
-        if current_part_one > part_one && current_part_two > part_two {
-            break;
-        }
-
-        part_one = part_one.min(current_part_one);
-        part_two = part_two.min(current_part_two);
-    }
-
-    (part_one, part_two)
+    (
+        binary_search(&crabs, left, right, simple_fuel_consumption),
+        binary_search(&crabs, left, right, summation_fuel_consumption),
+    )
 }
 
 fn time_it<F, T>(fun: F) -> T
