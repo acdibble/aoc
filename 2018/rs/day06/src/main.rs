e8f3a1be5a4d3561b4deaa8ab8b08d809fc4fb1c
Andrew Dibble <andrew@dibble.codes>
2021-12-15 20:32:04 +0100

    [rs] Day 6 2018 update

---
+++ 2018/rs/day06/src/main.rs
@@ -0,0 +1,155 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::time::SystemTime;
+
+#[derive(Debug, Default)]
+pub struct Coordinate {
+    x: i32,
+    y: i32,
+    pub count: i32,
+}
+
+impl Coordinate {
+    pub fn from_string(string: &str) -> Coordinate {
+        let mut coord: Coordinate = Default::default();
+        let mut x_set = false;
+        for part in string.split(", ") {
+            if x_set {
+                coord.y = part.parse().unwrap();
+            } else {
+                coord.x = part.parse().unwrap();
+            }
+            x_set = true;
+        }
+        coord
+    }
+
+    #[allow(dead_code)]
+    pub fn distance_to(&self, x: i32, y: i32) -> i32 {
+        (self.x - x).abs() + (self.y - y).abs()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct CoordinateMap {
+    pub leftmost: Option<i32>,
+    pub rightmost: Option<i32>,
+    pub topmost: Option<i32>,
+    pub bottommost: Option<i32>,
+    pub coords: Vec<Coordinate>,
+}
+
+impl CoordinateMap {
+    pub fn new() -> CoordinateMap {
+        Default::default()
+    }
+
+    pub fn add(&mut self, coord_str: &str) -> &mut CoordinateMap {
+        let coord = Coordinate::from_string(coord_str);
+
+        self.leftmost = if self.leftmost.unwrap_or(999999) > coord.x {
+            Some(coord.x)
+        } else {
+            self.leftmost
+        };
+
+        self.topmost = if self.topmost.unwrap_or(0) < coord.y {
+            Some(coord.x)
+        } else {
+            self.topmost
+        };
+
+        self.bottommost = if self.bottommost.unwrap_or(999999) > coord.y {
+            Some(coord.x)
+        } else {
+            self.bottommost
+        };
+
+        self.rightmost = if self.rightmost.unwrap_or(0) < coord.x {
+            Some(coord.x)
+        } else {
+            self.rightmost
+        };
+
+        self.coords.push(coord);
+
+        self
+    }
+}
+
+fn part_one(input: &str) -> i32 {
+    let mut map = input.lines().fold(CoordinateMap::new(), |mut acc, line| {
+        acc.add(line);
+        acc
+    });
+
+    for i in map.leftmost.unwrap()..=map.rightmost.unwrap() {
+        for j in map.bottommost.unwrap()..=map.topmost.unwrap() {
+            let mut tie = false;
+            let mut shortest_distance = 999999;
+            let mut closest: Option<&mut Coordinate> = None;
+
+            for coord in map.coords.iter_mut() {
+                let current_distance = coord.distance_to(i, j);
+                if current_distance < shortest_distance {
+                    shortest_distance = current_distance;
+                    closest = Some(coord);
+                    tie = false;
+                } else if shortest_distance == current_distance {
+                    tie = true;
+                }
+            }
+
+            if !tie {
+                closest.unwrap().count += 1;
+            }
+        }
+    }
+
+    map.coords.iter().max_by_key(|c| c.count).unwrap().count
+}
+
+fn part_two(input: &str) -> i32 {
+    let map = input.lines().fold(CoordinateMap::new(), |mut acc, line| {
+        acc.add(line);
+        acc
+    });
+
+    let mut safe_squares = 0;
+
+    for i in 0..=map.rightmost.unwrap() {
+        for j in 0..=map.topmost.unwrap() {
+            safe_squares += match map
+                .coords
+                .iter()
+                .fold(0, |sum, coord| sum + coord.distance_to(i, j))
+            {
+                n if n < 10000 => 1,
+                _ => 0,
+            }
+        }
+    }
+
+    safe_squares
+}
+
+fn time_it<F, T>(fun: F) -> T
+where
+    F: Fn() -> T,
+{
+    let start = SystemTime::now();
+    let result = fun();
+    println!("Time elapsed: {} µs", start.elapsed().unwrap().as_micros());
+    result
+}
+
+fn main() -> std::io::Result<()> {
+    let file_path = env::current_dir()?.join(Path::new("data.txt"));
+    let input = fs::read_to_string(file_path)?;
+
+    time_it(|| println!("part 1: {}", part_one(&input)));
+    time_it(|| println!("part 2: {}", part_two(&input)));
+
+    Ok(())
+}
