6ba2e49bb91d5f6dd3a109473e38f00d90e39645
Andrew Dibble <andrew@dibble.codes>
2021-12-26 16:16:55 +0100

    [rs] Day 14 2018

--- 2018/rs/day14/src/main.rs
+++ 2018/rs/day14/src/main.rs
@@ -3,12 +3,84 @@ use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
 
-fn part_one(input: &str) -> i32 {
-    0
+fn part_one(input: &str) -> String {
+    let mut recipes = vec![3, 7];
+
+    let mut elf_one = 0usize;
+    let mut elf_two = 1usize;
+
+    let amount = input.parse().unwrap();
+
+    while recipes.len() < amount + 10 {
+        match (recipes.get(elf_one), recipes.get(elf_two)) {
+            (Some(&value_one), Some(&value_two)) => {
+                let mut sum = value_one + value_two;
+
+                match sum / 10 {
+                    0 => (),
+                    value => {
+                        recipes.push(value);
+                        sum %= 10;
+                    }
+                }
+
+                recipes.push(sum);
+                elf_one = (elf_one + 1 + value_one) % recipes.len();
+                elf_two = (elf_two + 1 + value_two) % recipes.len();
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    recipes[amount..(amount + 10)]
+        .iter()
+        .flat_map(|v| char::from_digit(*v as u32, 10))
+        .collect()
 }
 
-fn part_two(input: &str) -> i32 {
-    0
+fn part_two(input: &str) -> usize {
+    let mut recipes = vec![3, 7];
+
+    let mut elf_one = 0usize;
+    let mut elf_two = 1usize;
+
+    let target: &[usize] = &input
+        .chars()
+        .map(|ch| ch.to_digit(10).unwrap() as usize)
+        .collect::<Vec<_>>()[..];
+    let target_len = target.len();
+
+    loop {
+        match (recipes.get(elf_one), recipes.get(elf_two)) {
+            (Some(&value_one), Some(&value_two)) => {
+                let mut sum = value_one + value_two;
+
+                match sum / 10 {
+                    0 => (),
+                    value => {
+                        recipes.push(value);
+                        sum %= 10;
+                    }
+                }
+
+                recipes.push(sum);
+                elf_one = (elf_one + 1 + value_one) % recipes.len();
+                elf_two = (elf_two + 1 + value_two) % recipes.len();
+            }
+            _ => unreachable!(),
+        }
+
+        let len = recipes.len();
+        if len > target_len + 1 {
+            if &recipes[len - target_len..len] == target {
+                return len - target_len;
+            }
+
+            if &recipes[len - target_len - 1..len - 1] == target {
+                return len - target_len - 1;
+            }
+        }
+    }
 }
 
 fn time_it<F, T>(fun: F) -> T
@@ -30,4 +102,3 @@ fn main() -> std::io::Result<()> {
 
     Ok(())
 }
-
