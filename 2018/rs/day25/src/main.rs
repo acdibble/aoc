f139843d1f43e7c199222592e0ef6804751da5cc
Andrew Dibble <andrew@dibble.codes>
2022-01-05 18:27:54 +0100

    [rs] Day 25 2018

--- 2018/rs/day25/src/main.rs
+++ 2018/rs/day25/src/main.rs
@@ -3,12 +3,56 @@ use std::fs;
 use std::path::Path;
 use std::time::SystemTime;
 
-fn part_one(input: &str) -> i32 {
-    0
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+struct Point(i32, i32, i32, i32);
+
+impl Point {
+    fn distance(&self, other: &Self) -> i32 {
+        (self.0 - other.0).abs()
+            + (self.1 - other.1).abs()
+            + (self.2 - other.2).abs()
+            + (self.3 - other.3).abs()
+    }
+
+    fn from_str(line: &str) -> Self {
+        let mut parts = line.split(',');
+        Self(
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+            parts.next().unwrap().parse().unwrap(),
+        )
+    }
 }
 
-fn part_two(input: &str) -> i32 {
-    0
+fn part_one(input: &str) -> usize {
+    let mut points: Vec<_> = input.lines().map(Point::from_str).collect();
+
+    let mut count = 0;
+
+    let mut constellation = vec![];
+    while points.len() != 0 {
+        let mut j = 0;
+        constellation.clear();
+        constellation.push(points.pop().unwrap());
+
+        while j < constellation.len() {
+            let mut i = 0;
+            while i < points.len() {
+                if points[i].distance(&constellation[j]) <= 3 {
+                    let point = points.swap_remove(i);
+                    constellation.push(point);
+                } else {
+                    i += 1;
+                }
+            }
+            j += 1;
+        }
+
+        count += 1;
+    }
+
+    count
 }
 
 fn time_it<F, T>(fun: F) -> T
@@ -26,8 +70,6 @@ fn main() -> std::io::Result<()> {
     let input = fs::read_to_string(file_path)?;
 
     time_it(|| println!("part 1: {}", part_one(&input)));
-    time_it(|| println!("part 2: {}", part_two(&input)));
 
     Ok(())
 }
-
