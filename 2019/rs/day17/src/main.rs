2c0bc69b861c3382d6d02a44a12925a687dbc5dc
Andrew Dibble <andrew@dibble.codes>
2022-07-29 17:45:21 +0200

    [rs] bootstrap 2019

---
+++ 2019/rs/day17/src/main.rs
@@ -0,0 +1,29 @@
+use std::{env, fs, path::Path, time::SystemTime};
+
+fn part_one(input: &str) -> i32 {
+    0
+}
+
+fn part_two(input: &str) -> i32 {
+    0
+}
+
+fn time_it<F, T>(fun: F) -> T
+where
+    F: Fn() -> T,
+{
+    let start = SystemTime::now();
+    let result = fun();
+    println!("Time elapsed: {} µs", start.elapsed().unwrap().as_micros());
+    result
+}
+
+fn main() -> std::io::Result<()> {
+    let file_path = env::current_dir()?.join(Path::new("data.txt"));
+    let input = fs::read_to_string(file_path)?;
+
+    time_it(|| println!("part 1: {}", part_one(&input)));
+    time_it(|| println!("part 1: {}", part_two(&input)));
+
+    Ok(())
+}
