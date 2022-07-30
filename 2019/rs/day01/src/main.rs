0723aab2e28036bd28dcff1cd85400fc43225a3e
Andrew Dibble <andrew@dibble.codes>
2022-07-30 15:35:48 +0200

    [rs] Day 1 2019

--- 2019/rs/day01/src/main.rs
+++ 2019/rs/day01/src/main.rs
@@ -1,11 +1,21 @@
 use std::{env, fs, path::Path, time::SystemTime};
 
-fn part_one(input: &str) -> i32 {
-    0
+fn part_one(input: &Vec<i32>) -> i32 {
+    input.iter().map(|x| x / 3 - 2).sum()
 }
 
-fn part_two(input: &str) -> i32 {
-    0
+fn calculate_cost(value: i32, total: i32) -> i32 {
+    let result = value / 3 - 2;
+
+    if result <= 0 {
+        total
+    } else {
+        calculate_cost(result, total + result)
+    }
+}
+
+fn part_two(input: &Vec<i32>) -> i32 {
+    input.iter().map(|x| calculate_cost(*x, 0)).sum()
 }
 
 fn time_it<F, T>(fun: F) -> T
@@ -21,9 +31,10 @@ where
 fn main() -> std::io::Result<()> {
     let file_path = env::current_dir()?.join(Path::new("data.txt"));
     let input = fs::read_to_string(file_path)?;
+    let numbers: Vec<i32> = input.split('\n').flat_map(|s| s.parse::<i32>()).collect();
 
-    time_it(|| println!("part 1: {}", part_one(&input)));
-    time_it(|| println!("part 1: {}", part_two(&input)));
+    time_it(|| println!("part 1: {}", part_one(&numbers)));
+    time_it(|| println!("part 1: {}", part_two(&numbers)));
 
     Ok(())
 }
