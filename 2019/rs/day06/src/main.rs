879b52181851cd5f1d6fdcd6030435478a5a2e7f
Andrew Dibble <andrew@dibble.codes>
2022-08-17 18:00:17 +0200

    [rs] Day 6 2019

--- 2019/rs/day06/src/main.rs
+++ 2019/rs/day06/src/main.rs
@@ -1,13 +1,68 @@
-use std::time::SystemTime;
+use std::{collections::HashMap, time::SystemTime};
 
 const DATA: &'static str = include_str!("../data.txt");
 
 fn part_one() -> i32 {
-    0
+    let mut graph = HashMap::<&str, Vec<&str>>::with_capacity(1500);
+
+    for line in DATA.trim().lines() {
+        let mut split = line.split(')');
+        let object = split.next().unwrap();
+        let satellite = split.next().unwrap();
+
+        let entry = graph.entry(object).or_insert_with(|| Vec::with_capacity(2));
+        entry.push(satellite);
+    }
+
+    let mut stack = Vec::with_capacity(500);
+    stack.push(("COM", 0));
+
+    let mut count = 0;
+
+    while let Some((current, depth)) = stack.pop() {
+        count += depth;
+        if let Some(children) = graph.get(current) {
+            for child in children {
+                stack.push((child, depth + 1));
+            }
+        }
+    }
+
+    count
 }
 
 fn part_two() -> i32 {
-    0
+    let mut orbit_map = HashMap::<&str, &str>::with_capacity(2000);
+
+    for line in DATA.trim().lines() {
+        let mut split = line.split(')');
+        let object = split.next().unwrap();
+        let satellite = split.next().unwrap();
+
+        orbit_map.insert(satellite, object);
+    }
+
+    let mut you_path = HashMap::<&str, i32>::new();
+
+    let mut current = orbit_map["YOU"];
+    let mut transfers = 0;
+
+    while current != "COM" {
+        you_path.insert(current, transfers);
+
+        current = orbit_map[current];
+        transfers += 1;
+    }
+
+    let mut current = orbit_map["SAN"];
+    let mut transfers = 0;
+
+    while you_path.get(current).is_none() {
+        transfers += 1;
+        current = orbit_map[current];
+    }
+
+    you_path[current] + transfers
 }
 
 fn time_it<F, T>(fun: F) -> T
