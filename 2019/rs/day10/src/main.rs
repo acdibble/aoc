3d1c0351db855e6b24f6be336b08a585e4127620
Andrew Dibble <andrew@dibble.codes>
2022-08-20 15:33:35 +0200

    [rs] Day 10 2019

--- 2019/rs/day10/src/main.rs
+++ 2019/rs/day10/src/main.rs
@@ -1,13 +1,152 @@
-use std::time::SystemTime;
+use std::{
+    cmp,
+    collections::{HashMap, HashSet},
+    time::SystemTime,
+};
+use utils::{fraction::Fraction, grid::Coordinate};
 
 const DATA: &'static str = include_str!("../data.txt");
 
-fn part_one() -> i32 {
-    0
+fn parse_asteroids() -> Vec<Coordinate> {
+    let mut asteroids = vec![];
+
+    for (y, line) in DATA.trim().lines().enumerate() {
+        for (x, ch) in line.char_indices() {
+            if ch == '#' {
+                asteroids.push(Coordinate::new(x as i32, y as i32));
+            }
+        }
+    }
+
+    asteroids
+}
+
+fn find_station(asteroids: &Vec<Coordinate>) -> (Coordinate, usize) {
+    let mut max = 0;
+    let mut slopes = HashSet::new();
+    let mut winner = Coordinate::new(0, 0);
+
+    for coord in asteroids {
+        slopes.clear();
+
+        for other in asteroids {
+            slopes.insert((other - coord).slope());
+        }
+
+        let count = slopes.len();
+
+        if count > max {
+            max = count;
+            winner = *coord;
+        }
+    }
+
+    (winner, max)
+}
+
+fn part_one() -> usize {
+    let asteroids = parse_asteroids();
+
+    find_station(&asteroids).1
 }
 
 fn part_two() -> i32 {
-    0
+    let asteroids = parse_asteroids();
+
+    let station = find_station(&asteroids).0;
+
+    let mut quadrant_maps = [
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+        HashMap::new(),
+    ];
+    let mut slopes = HashSet::new();
+
+    for asteroid in asteroids {
+        if asteroid == station {
+            continue;
+        }
+
+        let mut coord = asteroid - station;
+        let quadrant: usize;
+
+        // (1,1)  |  (3,1)
+        // -----(2,2)-----
+        // (1,3)  |  (3,3)
+        //
+        // (-1,-1)  |  (1,-1)
+        // -------(0,0)------
+        // (-1,1)   |  (1,1)
+
+        match (coord.x.cmp(&0), coord.y.cmp(&0)) {
+            (cmp::Ordering::Equal | cmp::Ordering::Greater, cmp::Ordering::Less) => {
+                quadrant = 0;
+            }
+            (cmp::Ordering::Greater, cmp::Ordering::Equal | cmp::Ordering::Greater) => {
+                quadrant = 1;
+                coord = coord.rotate_left();
+            }
+            (cmp::Ordering::Less | cmp::Ordering::Equal, cmp::Ordering::Greater) => {
+                quadrant = 2;
+                coord = coord.rotate_left().rotate_left();
+            }
+            (cmp::Ordering::Less, cmp::Ordering::Less | cmp::Ordering::Equal) => {
+                quadrant = 3;
+                coord = coord.rotate_right();
+            }
+            (cmp::Ordering::Equal, cmp::Ordering::Equal) => unreachable!(),
+        }
+
+        slopes.insert(coord.slope());
+
+        quadrant_maps[quadrant]
+            .entry(coord.slope())
+            .or_insert_with(|| Vec::new())
+            .push(coord)
+    }
+
+    let mut slopes = slopes.into_iter().collect::<Vec<_>>();
+    slopes.sort_by(|a, b| {
+        if matches!(a, Fraction::PositiveUndefined) {
+            cmp::Ordering::Less
+        } else if matches!(b, Fraction::PositiveUndefined) {
+            cmp::Ordering::Greater
+        } else {
+            a.cmp(&b)
+        }
+    });
+
+    for map in &mut quadrant_maps {
+        for (_, vec) in map.iter_mut() {
+            vec.sort_by(|a, b| {
+                b.manhattan_distance_to_origin()
+                    .cmp(&a.manhattan_distance_to_origin())
+            })
+        }
+    }
+
+    let mut destroyed = 0;
+
+    loop {
+        for (quadrant, map) in quadrant_maps.iter_mut().enumerate() {
+            for slope in &slopes {
+                if let Some(vec) = map.get_mut(slope) {
+                    if let Some(el) = vec.pop() {
+                        destroyed += 1;
+                        if destroyed == 200 {
+                            let mut result = el;
+                            for _ in 0..quadrant {
+                                result = result.rotate_right();
+                            }
+                            result += station;
+                            return result.x * 100 + result.y;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 fn time_it<F, T>(fun: F) -> T
--- 2019/rs/utils/src/lib.rs
+++ 2019/rs/utils/src/lib.rs
@@ -43,3 +43,292 @@ pub fn permute<const N: usize, T: Copy>(input: [T; N]) -> impl Iterator<Item = [
         initial_done: false,
     }
 }
+
+// Euclid's two-thousand-year-old algorithm for finding the greatest common
+// divisor.
+fn gcd(x: i32, y: i32) -> i32 {
+    let mut x = x.abs();
+    let mut y = y.abs();
+    while y != 0 {
+        let t = y;
+        y = x % y;
+        x = t;
+    }
+    x
+}
+
+pub mod fraction {
+    #[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
+    pub enum Fraction {
+        Defined(i32, i32),
+        NegativeUndefined,
+        PositiveUndefined,
+    }
+
+    impl Into<f64> for &Fraction {
+        fn into(self) -> f64 {
+            match self {
+                &Fraction::Defined(num, denom) => num as f64 / denom as f64,
+                Fraction::PositiveUndefined => f64::INFINITY,
+                Fraction::NegativeUndefined => f64::NEG_INFINITY,
+            }
+        }
+    }
+
+    impl Into<f64> for Fraction {
+        fn into(self) -> f64 {
+            match self {
+                Self::Defined(num, denom) => num as f64 / denom as f64,
+                Self::PositiveUndefined => f64::INFINITY,
+                Self::NegativeUndefined => f64::NEG_INFINITY,
+            }
+        }
+    }
+
+    impl Ord for Fraction {
+        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+            if self == other {
+                return std::cmp::Ordering::Equal;
+            }
+
+            let a: f64 = self.into();
+            let b: f64 = other.into();
+
+            if a < b {
+                std::cmp::Ordering::Less
+            } else if a > b {
+                std::cmp::Ordering::Greater
+            } else {
+                match (self.reduce(), other.reduce()) {
+                    (Self::Defined(num_a, den_a), Self::Defined(num_b, den_b)) => {
+                        match num_a.cmp(&num_b) {
+                            std::cmp::Ordering::Equal => den_a.cmp(&den_b),
+                            other => other,
+                        }
+                    }
+                    _ => std::cmp::Ordering::Equal,
+                }
+            }
+        }
+    }
+
+    impl PartialOrd for Fraction {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl Fraction {
+        pub fn from(numerator: i32, denominator: i32) -> Self {
+            if denominator == 0 {
+                return if numerator.is_negative() {
+                    Self::NegativeUndefined
+                } else {
+                    Self::PositiveUndefined
+                };
+            }
+
+            Self::Defined(numerator, denominator)
+        }
+
+        pub fn reduce(&self) -> Self {
+            match self {
+                &Self::Defined(numerator, denominator) => {
+                    let divisor = super::gcd(numerator, denominator);
+
+                    Self::Defined(numerator / divisor, denominator / divisor)
+                }
+                _ => *self,
+            }
+        }
+
+        pub fn normalize(&self) -> Self {
+            use std::cmp::Ordering::*;
+
+            match self {
+                &Self::Defined(mut numerator, mut denominator) => {
+                    match (numerator.cmp(&0), denominator.cmp(&0)) {
+                        (Greater, Less) | (Less, Less) => {
+                            numerator = -numerator;
+                            denominator = -denominator;
+                        }
+                        _ => {}
+                    }
+
+                    Self::Defined(numerator, denominator)
+                }
+                _ => *self,
+            }
+        }
+
+        pub fn normalized(numerator: i32, denominator: i32) -> Self {
+            Self::from(numerator, denominator).reduce().normalize()
+        }
+    }
+}
+
+pub mod grid {
+    use super::fraction::Fraction;
+
+    #[derive(Debug, Eq, PartialEq, Copy, Clone)]
+    pub struct Coordinate {
+        pub x: i32,
+        pub y: i32,
+    }
+
+    impl Coordinate {
+        pub fn new(x: i32, y: i32) -> Self {
+            Self { x, y }
+        }
+
+        pub fn rotate_right(&self) -> Self {
+            Self {
+                x: self.y,
+                y: -self.x,
+            }
+        }
+
+        pub fn rotate_left(&self) -> Self {
+            Self {
+                x: -self.y,
+                y: self.x,
+            }
+        }
+
+        pub fn slope(&self) -> Fraction {
+            Fraction::from(self.y, self.x).reduce()
+        }
+
+        pub fn manhattan_distance(&self, other: &Self) -> i32 {
+            (self.x - other.x).abs() + (self.y - other.y).abs()
+        }
+
+        pub fn manhattan_distance_to_origin(&self) -> i32 {
+            self.manhattan_distance(&Coordinate::new(0, 0))
+        }
+    }
+
+    impl std::ops::Sub<Coordinate> for Coordinate {
+        type Output = Coordinate;
+
+        fn sub(self, rhs: Coordinate) -> Self::Output {
+            let Coordinate { x, y } = self;
+            let Coordinate {
+                x: other_x,
+                y: other_y,
+            } = rhs;
+            Coordinate {
+                x: x - other_x,
+                y: y - other_y,
+            }
+        }
+    }
+
+    impl std::ops::Sub<&Coordinate> for &Coordinate {
+        type Output = Coordinate;
+
+        fn sub(self, rhs: &Coordinate) -> Self::Output {
+            *self - *rhs
+        }
+    }
+
+    impl std::ops::Add<Coordinate> for Coordinate {
+        type Output = Coordinate;
+
+        fn add(self, rhs: Coordinate) -> Self::Output {
+            let Coordinate { x, y } = self;
+            let Coordinate {
+                x: other_x,
+                y: other_y,
+            } = rhs;
+            Coordinate {
+                x: x + other_x,
+                y: y + other_y,
+            }
+        }
+    }
+
+    impl std::ops::Add<&Coordinate> for &Coordinate {
+        type Output = Coordinate;
+
+        fn add(self, rhs: &Coordinate) -> Self::Output {
+            *self + *rhs
+        }
+    }
+
+    impl std::ops::AddAssign<Coordinate> for Coordinate {
+        fn add_assign(&mut self, rhs: Coordinate) {
+            self.x += rhs.x;
+            self.y += rhs.y;
+        }
+    }
+
+    impl std::fmt::Display for Coordinate {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "({}, {})", self.x, self.y)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::grid::Coordinate;
+
+    use super::{fraction::*, *};
+
+    #[test]
+    fn test_fraction() {
+        assert_eq!(Fraction::normalized(-1, -1), Fraction::from(1, 1));
+        assert_eq!(Fraction::normalized(-1, 1), Fraction::from(-1, 1));
+        assert_eq!(Fraction::normalized(1, 1), Fraction::from(1, 1));
+        assert_eq!(Fraction::normalized(1, -1), Fraction::from(-1, 1));
+        assert_eq!(Fraction::normalized(2, -2), Fraction::from(-1, 1));
+        assert_eq!(Fraction::normalized(30, 5), Fraction::from(6, 1));
+        assert_eq!(Fraction::normalized(5, 50), Fraction::from(1, 10));
+        assert_eq!(Fraction::normalized(1, 0), Fraction::PositiveUndefined);
+        assert_eq!(Fraction::normalized(-1, 0), Fraction::NegativeUndefined);
+        assert_eq!(Fraction::PositiveUndefined, Fraction::PositiveUndefined);
+        assert_eq!(Fraction::NegativeUndefined, Fraction::NegativeUndefined);
+    }
+
+    #[test]
+    fn test_coordinate() {
+        let coord = Coordinate::new(1, 2);
+
+        assert_eq!(coord.rotate_right(), Coordinate::new(2, -1));
+        assert_eq!(coord.rotate_right().rotate_right(), Coordinate::new(-1, -2));
+        assert_eq!(
+            coord.rotate_right().rotate_right().rotate_right(),
+            Coordinate::new(-2, 1)
+        );
+        assert_eq!(
+            coord
+                .rotate_right()
+                .rotate_right()
+                .rotate_right()
+                .rotate_right(),
+            Coordinate::new(1, 2)
+        );
+        assert_eq!(coord.rotate_left(), Coordinate::new(-2, 1));
+        assert_eq!(coord.rotate_left().rotate_left(), Coordinate::new(-1, -2));
+        assert_eq!(
+            coord.rotate_left().rotate_left().rotate_left(),
+            Coordinate::new(2, -1)
+        );
+        assert_eq!(
+            coord
+                .rotate_left()
+                .rotate_left()
+                .rotate_left()
+                .rotate_left(),
+            Coordinate::new(1, 2)
+        );
+
+        assert_eq!(Coordinate::new(2, 6).slope(), Fraction::normalized(3, 1));
+
+        assert_eq!(
+            Coordinate::new(2, 6).manhattan_distance(&Coordinate::new(0, 0)),
+            8
+        )
+    }
+}
