01eeb379149321ed8414c6dd3fb262e3006f914f
Andrew Dibble <andrew@dibble.codes>
2022-12-06 10:02:17 +0100

    [rs] Day 6 2022

--- 2022/rs/day06/src/main.rs
+++ 2022/rs/day06/src/main.rs
@@ -2,12 +2,66 @@ use std::time::SystemTime;
 
 const DATA: &'static str = include_str!("../data.txt");
 
-fn part_one() -> i32 {
-    0
+struct Buffer<const N: usize> {
+    chars: [char; N],
+    index: usize,
+    count: usize,
 }
 
-fn part_two() -> i32 {
-    0
+impl<const N: usize> Buffer<N> {
+    fn new() -> Self {
+        Self {
+            chars: ['\0'; N],
+            index: 0,
+            count: 0,
+        }
+    }
+
+    fn add(&mut self, ch: char) {
+        let index = self.index;
+        self.chars[index] = ch;
+        self.index = (self.index + 1) % N;
+        self.count += 1;
+    }
+
+    fn check(&self) -> Option<usize> {
+        for (index, ch) in self.chars.iter().enumerate() {
+            for other in self.chars.iter().skip(index + 1) {
+                if other == ch {
+                    return None;
+                }
+            }
+        }
+
+        Some(self.count)
+    }
+}
+
+fn find_unique_sequence<const N: usize>() -> usize {
+    let mut chars = DATA.chars();
+
+    let mut buffer = Buffer::<N>::new();
+
+    for _ in 0..N {
+        buffer.add(chars.next().unwrap());
+    }
+
+    for ch in chars {
+        buffer.add(ch);
+        if let Some(result) = buffer.check() {
+            return result;
+        }
+    }
+
+    unreachable!()
+}
+
+fn part_one() -> usize {
+    find_unique_sequence::<4>()
+}
+
+fn part_two() -> usize {
+    find_unique_sequence::<14>()
 }
 
 fn time_it<F, T>(fun: F) -> T
