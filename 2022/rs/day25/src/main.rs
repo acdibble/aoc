32a1e4da070d3c482af024fa71663a456a16a295
Andrew Dibble <andrew@dibble.codes>
2023-01-03 23:34:28 +0100

    [rs] Day 25 2022

--- 2022/rs/day25/src/main.rs
+++ 2022/rs/day25/src/main.rs
@@ -1,13 +1,103 @@
-use std::time::SystemTime;
+use std::{fmt::Display, ops::Add, time::SystemTime};
 
 const DATA: &'static str = include_str!("../data.txt");
 
-fn part_one() -> i32 {
-    0
+#[derive(Debug)]
+struct Snafu {
+    digits: Vec<i64>,
 }
 
-fn part_two() -> i32 {
-    0
+impl Snafu {
+    fn from(digits: &str) -> Self {
+        Self {
+            digits: digits
+                .chars()
+                .map(|ch| match ch {
+                    '2' => 2,
+                    '1' => 1,
+                    '0' => 0,
+                    '-' => -1,
+                    '=' => -2,
+                    _ => unreachable!(),
+                })
+                .collect(),
+        }
+    }
+}
+
+impl Add<Snafu> for Snafu {
+    type Output = Self;
+
+    fn add(self, other: Snafu) -> Self::Output {
+        let mut result = Snafu {
+            digits: Vec::with_capacity(self.digits.len().max(other.digits.len())),
+        };
+
+        let mut iter_a = self.digits.iter().rev();
+        let mut iter_b = other.digits.iter().rev();
+        let mut carry = 0;
+
+        let mut can_loop = true;
+        while can_loop {
+            let mut sum = match (iter_a.next(), iter_b.next()) {
+                (Some(a), Some(b)) => a + b,
+                (None, Some(value)) | (Some(value), None) => *value,
+                (None, None) => {
+                    can_loop = false;
+                    0
+                }
+            } + carry;
+
+            carry = if sum > 2 {
+                sum -= 5;
+                1
+            } else if sum < -2 {
+                sum += 5;
+                -1
+            } else {
+                0
+            };
+
+            if can_loop || sum != 0 {
+                result.digits.push(sum);
+            }
+        }
+
+        result.digits.reverse();
+
+        result
+    }
+}
+
+impl Display for Snafu {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.digits.len() == 0 {
+            write!(f, "0")?;
+        } else {
+            for digit in &self.digits {
+                write!(
+                    f,
+                    "{}",
+                    match *digit {
+                        -2 => '=',
+                        -1 => '-',
+                        0 => '0',
+                        1 => '1',
+                        2 => '2',
+                        _ => unreachable!(),
+                    }
+                )?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+fn part_one() -> Snafu {
+    DATA.lines()
+        .map(Snafu::from)
+        .fold(Snafu::from("0"), |acc, snafu| acc + snafu)
 }
 
 fn time_it<F, T>(fun: F) -> T
@@ -22,5 +112,4 @@ where
 
 fn main() {
     time_it(|| println!("part 1: {}", part_one()));
-    time_it(|| println!("part 2: {}", part_two()));
 }
