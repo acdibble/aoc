de40fffcf78b660d0a808ecc87104dbf156423b5
Andrew Dibble <andrew@dibble.codes>
2023-01-08 17:21:48 +0100

    [rs] Day 22 2022 part 2

--- 2022/rs/day22/src/main.rs
+++ 2022/rs/day22/src/main.rs
@@ -3,7 +3,7 @@ use utils::{Chart, Coord};
 
 const DATA: &'static str = include_str!("../data.txt");
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Tile {
     Empty,
     Open,
@@ -74,18 +74,18 @@ enum Instruction {
 }
 
 impl Instruction {
-    fn apply(&self, tile: &Tile) -> Tile {
+    fn apply(&self, tile: &Tile) -> Option<Tile> {
         match self {
-            Self::Right => tile.rotated_right(),
-            Self::Left => tile.rotated_left(),
-            _ => unreachable!(),
+            Self::Right => Some(tile.rotated_right()),
+            Self::Left => Some(tile.rotated_left()),
+            _ => None,
         }
     }
 
-    fn amount(&self) -> i64 {
+    fn amount(&self) -> Option<i64> {
         match self {
-            Self::Amount(amount) => *amount,
-            _ => unreachable!(),
+            Self::Amount(amount) => Some(*amount),
+            _ => None,
         }
     }
 }
@@ -107,10 +107,7 @@ impl Into<char> for Tile {
             Self::Empty => ' ',
             Self::Open => '.',
             Self::Wall => '#',
-            Self::Up => '^',
-            Self::Right => '>',
-            Self::Down => 'v',
-            Self::Left => '<',
+            _ => unreachable!(),
         }
     }
 }
@@ -144,7 +141,44 @@ fn parse_instructions(line: &str) -> Vec<Instruction> {
     result
 }
 
-fn part_one() -> i64 {
+fn find_new_location_2d(
+    chart: &Chart<Tile>,
+    location: &Coord,
+    current: &Tile,
+) -> Option<(Coord, Tile)> {
+    let mut new_location = current.move_coord(*location);
+    match chart.get(&new_location).copied().unwrap_or_default() {
+        Tile::Empty => {
+            new_location = match current {
+                Tile::Up => Coord(location.0, chart.bottom()),
+                Tile::Right => Coord(chart.left(), location.1),
+                Tile::Down => Coord(location.0, chart.top()),
+                Tile::Left => Coord(chart.right(), location.1),
+                _ => unreachable!(),
+            };
+        }
+        Tile::Wall => return None,
+        _ => {}
+    }
+
+    while matches!(
+        chart.get(&new_location).copied().unwrap_or_default(),
+        Tile::Empty
+    ) {
+        new_location = current.move_coord(new_location);
+    }
+
+    if !chart.get(&new_location).unwrap().is_passable() {
+        return None;
+    }
+
+    Some((new_location, *current))
+}
+
+fn run<F>(find_new_location: F) -> i64
+where
+    F: Fn(&Chart<Tile>, &Coord, &Tile) -> Option<(Coord, Tile)>,
+{
     let mut chart: Chart<Tile> = Chart::new();
 
     let mut lines = DATA.lines().enumerate();
@@ -170,61 +204,65 @@ fn part_one() -> i64 {
 
     let instructions = parse_instructions(lines.next().unwrap().1);
     let mut location = start.unwrap();
-    chart.overwrite(&location, Tile::Right);
     let mut instructions = instructions.into_iter();
 
+    let mut current = Tile::Right;
+
     while let Some(inst) = instructions.next() {
-        let current = chart.get(&location).copied().unwrap_or_default();
-        chart.overwrite(&location, current);
-        for _ in 0..inst.amount() {
-            let next_location = current.move_coord(location);
-            let next_tile = chart.get(&next_location).copied().unwrap_or_default();
-
-            if next_tile.is_passable() {
-                location = next_location;
-                chart.overwrite(&location, current);
-                continue;
-            }
-
-            if matches!(next_tile, Tile::Wall) {
-                break;
-            }
-
-            let mut new_location = match current {
-                Tile::Up => Coord(location.0, chart.bottom()),
-                Tile::Right => Coord(chart.left(), location.1),
-                Tile::Down => Coord(location.0, chart.top()),
-                Tile::Left => Coord(chart.right(), location.1),
-                _ => unreachable!(),
-            };
-
-            while matches!(
-                chart.get(&new_location).copied().unwrap_or_default(),
-                Tile::Empty
-            ) {
-                new_location = current.move_coord(new_location);
-            }
-
-            let new_tile = chart.get(&new_location).unwrap();
-            if !new_tile.is_passable() {
-                break;
-            }
-
-            location = new_location;
-            chart.overwrite(&location, current);
+        if let Some(new_tile) = inst.apply(&current) {
+            current = new_tile;
+            continue;
         }
 
-        if let Some(inst) = instructions.next() {
-            let current = inst.apply(&current);
-            chart.overwrite(&location, current);
+        for _ in 0..inst.amount().unwrap() {
+            if let Some(result) = find_new_location(&chart, &location, &current) {
+                location = result.0;
+                current = result.1;
+            } else {
+                break;
+            }
         }
     }
 
-    1000 * (location.1 + 1) + 4 * (location.0 + 1) + chart.get(&location).unwrap().value()
+    1000 * (location.1 + 1) + 4 * (location.0 + 1) + current.value()
 }
 
-fn part_two() -> i32 {
-    0
+fn part_one() -> i64 {
+    run(&find_new_location_2d)
+}
+
+fn find_new_location_3d(
+    chart: &Chart<Tile>,
+    location: &Coord,
+    current: &Tile,
+) -> Option<(Coord, Tile)> {
+    let (new_location, new_direction) = match (location, current) {
+        (Coord(149, y @ 0..=49), &Tile::Right) => (Coord(99, 49 - y + 100), Tile::Left),
+        (Coord(x @ 100..=149, 49), &Tile::Down) => (Coord(99, x - 50), Tile::Left),
+        (Coord(99, y @ 50..=99), &Tile::Right) => (Coord(y + 50, 49), Tile::Up),
+        (Coord(99, y @ 100..=149), &Tile::Right) => (Coord(149, 149 - y), Tile::Left),
+        (Coord(x @ 50..=99, 149), &Tile::Down) => (Coord(49, x + 100), Tile::Left),
+        (Coord(49, y @ 150..=199), &Tile::Right) => (Coord(y - 100, 149), Tile::Up),
+        (Coord(x @ 0..=49, 199), &Tile::Down) => (Coord(x + 100, 0), Tile::Down),
+        (Coord(0, y @ 150..=199), &Tile::Left) => (Coord(y - 100, 0), Tile::Down),
+        (Coord(0, y @ 100..=149), &Tile::Left) => (Coord(50, 149 - y), Tile::Right),
+        (Coord(x @ 0..=49, 100), &Tile::Up) => (Coord(50, x + 50), Tile::Right),
+        (Coord(50, y @ 50..=99), &Tile::Left) => (Coord(y - 50, 100), Tile::Down),
+        (Coord(50, y @ 0..=49), &Tile::Left) => (Coord(0, 49 - y + 100), Tile::Right),
+        (Coord(x @ 50..=99, 0), &Tile::Up) => (Coord(0, x + 100), Tile::Right),
+        (Coord(x @ 100..=149, 0), &Tile::Up) => (Coord(x - 100, 199), Tile::Up),
+        _ => (current.move_coord(*location), *current),
+    };
+
+    match chart.get(&new_location) {
+        Some(Tile::Open) => Some((new_location, new_direction)),
+        Some(Tile::Wall) => None,
+        _ => unreachable!(),
+    }
+}
+
+fn part_two() -> i64 {
+    run(&find_new_location_3d)
 }
 
 fn time_it<F, T>(fun: F) -> T
