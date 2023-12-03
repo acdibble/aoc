f8b46a88879e31eed22204f4b673aa5649046663
Andrew Dibble <andrew@dibble.codes>
2023-12-03 22:21:24 +0100

    [rs] Day 2 2023

--- 2023/rs/day02/src/main.rs
+++ 2023/rs/day02/src/main.rs
@@ -3,11 +3,72 @@ use std::time::SystemTime;
 const DATA: &'static str = include_str!("../data.txt");
 
 fn part_one() -> i32 {
-    0
+    let mut sum = 0;
+
+    'game: for line in DATA.trim().lines() {
+        let mut it = line.split(": ");
+
+        let game = it
+            .next()
+            .unwrap()
+            .split(" ")
+            .last()
+            .unwrap()
+            .parse::<i32>()
+            .unwrap();
+
+        for rounds in it.next().unwrap().split("; ") {
+            for round in rounds.split(", ") {
+                println!("round: {round}");
+                let mut it = round.split(" ");
+
+                let count = it.next().unwrap().parse::<i32>().unwrap();
+                let color = it.next().unwrap();
+
+                match color {
+                    "red" if count > 12 => continue 'game,
+                    "green" if count > 13 => continue 'game,
+                    "blue" if count > 14 => continue 'game,
+                    _ => {}
+                }
+            }
+        }
+
+        sum += game
+    }
+
+    sum
 }
 
 fn part_two() -> i32 {
-    0
+    let mut sum = 0;
+
+    for line in DATA.trim().lines() {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        for rounds in line.split(": ").last().unwrap().split("; ") {
+            for round in rounds.split(", ") {
+                println!("round: {round}");
+                let mut it = round.split(" ");
+
+                let count = it.next().unwrap().parse::<i32>().unwrap();
+                let color = it.next().unwrap();
+
+                match color {
+                    "red" => red = red.max(count),
+                    "green" => green = green.max(count),
+                    "blue" => blue = blue.max(count),
+                    _ => unreachable!(),
+                }
+            }
+        }
+
+        sum += red * green * blue;
+    }
+
+    sum
 }
 
 fn time_it<F, T>(fun: F) -> T
