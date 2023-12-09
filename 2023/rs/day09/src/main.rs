0611e6aa2447d140c0e945c58d0752045e6f4215
Andrew Dibble <andrew@dibble.codes>
2023-12-09 15:12:48 +0100

    [rs] Day 9 2023

--- 2023/rs/day09/src/main.rs
+++ 2023/rs/day09/src/main.rs
@@ -2,12 +2,45 @@ use std::time::SystemTime;
 
 const DATA: &'static str = include_str!("../data.txt");
 
+fn parse_sequences() -> Vec<Vec<i32>> {
+    DATA.trim()
+        .lines()
+        .map(|l| l.split_ascii_whitespace().flat_map(|n| n.parse()).collect())
+        .collect()
+}
+
+fn get_differences(seq: &Vec<i32>) -> Vec<i32> {
+    seq.windows(2).map(|window| window[1] - window[0]).collect()
+}
+
+fn build_sequences(start: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut current = get_differences(&start);
+    let mut subsequences = vec![start];
+
+    loop {
+        let next = get_differences(&current);
+        subsequences.push(current);
+        if next.iter().all(|n| *n == 0) {
+            break;
+        }
+        current = next;
+    }
+
+    subsequences
+}
+
+fn calculate_total<F: Copy + Fn(i32, Vec<i32>) -> i32>(fun: F) -> i32 {
+    parse_sequences().into_iter().fold(0, |total, seq| {
+        total + build_sequences(seq).into_iter().rev().fold(0, fun)
+    })
+}
+
 fn part_one() -> i32 {
-    0
+    calculate_total(|acc, seq| acc + seq.last().unwrap())
 }
 
 fn part_two() -> i32 {
-    0
+    calculate_total(|acc, seq| seq.first().unwrap() - acc)
 }
 
 fn time_it<F, T>(fun: F) -> T
