ffa3276c44f3d7adbcc59bac3d355f4718093661
Andrew Dibble <andrew@dibble.codes>
2023-12-23 14:24:36 +0100

    [rs] Day 12 2023

--- 2023/rs/day12/src/main.rs
+++ 2023/rs/day12/src/main.rs
@@ -1,13 +1,102 @@
-use std::time::SystemTime;
+use std::{collections::BTreeMap, time::SystemTime};
 
 const DATA: &'static str = include_str!("../data.txt");
 
-fn part_one() -> i32 {
-    0
+const DAMAGED: char = '#';
+const SPRING: char = '.';
+
+fn place_spring(
+    cache: &mut BTreeMap<(usize, usize), usize>,
+    schema: &[char],
+    groups: &[usize],
+) -> usize {
+    let cache_key = (schema.len(), groups.len());
+    if let Some(count) = cache.get(&cache_key) {
+        return *count;
+    }
+
+    if groups.is_empty() {
+        let result = if schema.contains(&DAMAGED) { 0 } else { 1 };
+        cache.insert(cache_key, result);
+        return result;
+    }
+
+    let size = groups[0];
+    let mut count = 0;
+
+    for index in 0..schema.len() {
+        if schema[0..index].contains(&DAMAGED) {
+            break;
+        }
+
+        let schema = &schema[index..];
+
+        if schema.len() < size {
+            break;
+        }
+
+        let section = &schema[..size];
+        if section.contains(&SPRING) || schema.get(size) == Some(&DAMAGED) {
+            continue;
+        }
+
+        let skip = size + 1;
+
+        if skip > schema.len() {
+            if groups.len() == 1 {
+                count += 1;
+            }
+
+            break;
+        }
+
+        let next_schema = &schema[skip..];
+        count += place_spring(cache, next_schema, &groups[1..])
+    }
+
+    cache.insert(cache_key, count);
+
+    count
 }
 
-fn part_two() -> i32 {
-    0
+fn solve_line(line: &'static str, unfold: bool) -> usize {
+    let mut parts = line.split_ascii_whitespace();
+    let schema_chars = parts.next().unwrap().chars();
+    let group_nums = parts
+        .next()
+        .unwrap()
+        .split(',')
+        .flat_map(|x| x.parse::<usize>());
+
+    let mut schema = Vec::new();
+    let mut groups = Vec::new();
+
+    let iterations = if unfold { 5 } else { 1 };
+
+    for it in 0..iterations {
+        if it != 0 {
+            schema.push('?');
+        }
+
+        for ch in schema_chars.clone() {
+            schema.push(ch);
+        }
+
+        for n in group_nums.clone() {
+            groups.push(n);
+        }
+    }
+
+    let mut cache = BTreeMap::new();
+    place_spring(&mut cache, &mut schema, &groups)
+}
+
+fn part_one() -> usize {
+    DATA.lines().map(|line| solve_line(line, false)).sum()
+}
+
+fn part_two() -> usize {
+    DATA.lines().map(|line| solve_line(line, true)).sum()
 }
 
 fn time_it<F, T>(fun: F) -> T
