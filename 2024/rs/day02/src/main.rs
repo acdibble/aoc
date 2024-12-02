d2b6f915ef2026d6f16939177b9daa9853e58e94
Andrew Dibble <andrew@dibble.codes>
2024-12-02 10:51:29 +0100

    [rs] Day 2 2024

--- 2024/rs/day02/src/main.rs
+++ 2024/rs/day02/src/main.rs
@@ -1,13 +1,63 @@
-use std::time::SystemTime;
+use std::{cmp::Ordering, time::SystemTime};
 
 const DATA: &'static str = include_str!("../data.txt");
 
+fn is_safe(report: &Vec<i32>) -> bool {
+    let mut prev = None::<i32>;
+    let mut dir = None::<Ordering>;
+
+    for &int in report {
+        if let Some(prev) = prev {
+            if let Some(dir) = dir {
+                if dir != prev.cmp(&int) {
+                    return false;
+                }
+            } else {
+                dir = Some(prev.cmp(&int));
+            }
+
+            if !(1..=3).contains(&(prev - int).abs()) {
+                return false;
+            }
+        }
+
+        prev = Some(int);
+    }
+
+    true
+}
+
+fn parse_reports() -> impl Iterator<Item = Vec<i32>> {
+    DATA.lines().map(|l| {
+        l.split_ascii_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect()
+    })
+}
+
 fn part_one() -> i32 {
-    0
+    parse_reports().fold(0, |acc, l| acc + if is_safe(&l) { 1 } else { 0 })
+}
+
+fn can_be_made_safe(report: Vec<i32>) -> bool {
+    if is_safe(&report) {
+        return true;
+    }
+
+    for i in 0..report.len() {
+        let mut new_report = report.clone();
+        new_report.remove(i);
+
+        if is_safe(&new_report) {
+            return true;
+        }
+    }
+
+    false
 }
 
 fn part_two() -> i32 {
-    0
+    parse_reports().fold(0, |acc, l| acc + if can_be_made_safe(l) { 1 } else { 0 })
 }
 
 fn time_it<F, T>(fun: F) -> T
