9ccb6aa83d4894a8fbbd38a9da426d731a6ad11c
Andrew Dibble <andrew@dibble.codes>
2024-12-03 16:46:02 +0100

    [rs] Day 3 2024

--- 2024/rs/day03/src/main.rs
+++ 2024/rs/day03/src/main.rs
@@ -1,13 +1,37 @@
+use regex::Regex;
 use std::time::SystemTime;
 
 const DATA: &'static str = include_str!("../data.txt");
 
 fn part_one() -> i32 {
-    0
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    re.captures_iter(DATA)
+        .map(|c| c.extract())
+        .map(|(_, [a, b])| a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap())
+        .sum()
 }
 
 fn part_two() -> i32 {
-    0
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|(do)(\(\))|(don't)(\(\))").unwrap();
+
+    let mut active = true;
+    let mut sum = 0;
+
+    for (_, [a, b]) in re.captures_iter(DATA).map(|c| c.extract()) {
+        println!("{a} {b}");
+        match a {
+            "do" => active = true,
+            "don't" => active = false,
+            _ => {
+                if active {
+                    sum = sum + a.parse::<i32>().unwrap() * b.parse::<i32>().unwrap();
+                }
+            }
+        }
+    }
+
+    sum
 }
 
 fn time_it<F, T>(fun: F) -> T
