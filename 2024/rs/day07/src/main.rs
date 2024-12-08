d2544740c18293c4c5929924875264be0d173bc0
Andrew Dibble <andrew@dibble.codes>
2024-12-08 13:55:42 +0100

    [rs] Day 7 2024

--- 2024/rs/day07/src/main.rs
+++ 2024/rs/day07/src/main.rs
@@ -2,12 +2,66 @@ use std::time::SystemTime;
 
 const DATA: &'static str = include_str!("../data.txt");
 
-fn part_one() -> i32 {
-    0
+fn concat(a: i64, b: i64) -> i64 {
+    let mut shift = 1;
+    let mut rem = b;
+    while rem > 0 {
+        shift *= 10;
+        rem /= 10;
+    }
+    a * shift + b
 }
 
-fn part_two() -> i32 {
-    0
+enum Op {
+    Add,
+    Mul,
+    Cat,
+}
+
+impl Op {
+    fn perform(&self, a: i64, b: i64) -> i64 {
+        match self {
+            Op::Add => a + b,
+            Op::Mul => a * b,
+            Op::Cat => concat(a, b),
+        }
+    }
+}
+
+fn solve(target: i64, values: &[i64], total: i64, ops: &[Op]) -> bool {
+    if values.len() == 0 {
+        return target == total;
+    }
+
+    ops.iter()
+        .find(|op| solve(target, &values[1..], op.perform(total, values[0]), ops))
+        .is_some()
+}
+
+fn check_line(line: &'static str, ops: &[Op]) -> Option<i64>
+where
+{
+    let mut split = line.split(": ");
+    let target = split.next().unwrap().parse().unwrap();
+
+    let values: Vec<i64> = split
+        .next()
+        .unwrap()
+        .split(' ')
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    solve(target, &values, 0, ops).then_some(target)
+}
+
+fn part_one() -> i64 {
+    let ops = [Op::Add, Op::Mul];
+    DATA.trim().lines().flat_map(|l| check_line(l, &ops)).sum()
+}
+
+fn part_two() -> i64 {
+    let ops = [Op::Add, Op::Mul, Op::Cat];
+    DATA.trim().lines().flat_map(|l| check_line(l, &ops)).sum()
 }
 
 fn time_it<F, T>(fun: F) -> T
