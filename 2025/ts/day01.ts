e155f019522f2c232e63df70ba0cdf3d7832fd08
Andrew Dibble <andrew@dibble.codes>
2025-12-02 11:37:47 +0100

    [ts] Day 2 2025

--- 2025/ts/day01.ts
+++ 2025/ts/day01.ts
@@ -3,47 +3,27 @@ const rotations = (await Deno.readTextFile('data01.txt'))
   .split('\n')
   .map((line) => Number.parseInt(line.replace('R', '').replace('L', '-')));
 
-const part1 = () => {
-  let dial = 50;
-  let result = 0;
+let dial = 50;
+let part1 = 0;
+let part2 = 0;
 
-  for (const step of rotations.map((n) => n % 100)) {
-    if (dial + step > 99) {
-      dial = dial + step - 100;
-    } else if (dial + step < 0) {
-      dial = 100 + (dial + step);
-    } else {
-      dial += step;
+for (const rotation of rotations) {
+  const change = Math.sign(rotation) * -1;
+
+  for (let remaining = rotation; remaining !== 0; remaining += change) {
+    dial += change;
+
+    if (dial > 99) {
+      dial = 0;
+    } else if (dial < 0) {
+      dial = 99;
     }
 
-    if (dial === 0) result += 1;
+    if (dial === 0) part2 += 1;
   }
 
-  return result;
-};
+  if (dial === 0) part1 += 1;
+}
 
-const part2 = () => {
-  let dial = 50;
-  let result = 0;
-
-  for (const rotation of rotations) {
-    const change = Math.sign(rotation) * -1;
-
-    for (let remaining = rotation; remaining !== 0; remaining += change) {
-      dial += change;
-
-      if (dial > 99) {
-        dial = 0;
-      } else if (dial < 0) {
-        dial = 99;
-      }
-
-      if (dial === 0) result += 1;
-    }
-  }
-
-  return result;
-};
-
-console.log('part 1:', part1());
-console.log('part 2:', part2());
+console.log('part 1:', part1);
+console.log('part 2:', part2);
---
+++ 2025/ts/day02.ts
@@ -0,0 +1,36 @@
+const ranges = (await Deno.readTextFile('data02.txt'))
+  .trim()
+  .split(',')
+  .map((r) => {
+    const [start, end] = r.split('-').map((n) => Number.parseInt(n));
+    return { start, end };
+  });
+
+let part1 = 0;
+let part2 = 0;
+
+const regexps1: Record<number, RegExp> = {};
+const regexps2: Record<number, RegExp> = {};
+
+for (const { start, end } of ranges) {
+  for (let current = start; current <= end; current++) {
+    const str = current.toString();
+    const half = str.toString().length / 2;
+
+    regexps1[half] ??= new RegExp(String.raw`^(\d{${half}})\1$`);
+    if (regexps1[half].test(str)) {
+      part1 += current;
+    }
+
+    for (let i = 1; i <= half; i++) {
+      if (str.length % i !== 0) continue;
+      regexps2[i] ??= new RegExp(String.raw`^(\d{${i}})\1+$`);
+      if (regexps2[i].test(str)) {
+        part2 += current;
+        break;
+      }
+    }
+  }
+}
+
+console.log({ part1, part2 });
